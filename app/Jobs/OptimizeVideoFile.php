c81d9fd398db20070fd960efbb654f3b4f6f3c6a
Olexiy <shevchuka@gmail.com>
2017-04-10 21:48:11 +0300

    change movie convert

--- app/Jobs/OptimizeVideoFile.php
+++ app/Jobs/OptimizeVideoFile.php
@@ -41,7 +41,8 @@ class OptimizeVideoFile implements ShouldQueue
         $in = public_path('uploads/' . $this->objectWithMovie->getMovieFileName());
         $out = public_path('uploads/' . $convertedVideoName);
 
-        $outputAfterCommandRun = `ffmpeg -y -i {$in} -acodec libmp3lame -ar 44100 -ac 1 -vcodec libx264 -vf scale=640:-1 -fs 50M {$out}`;
+//        $outputAfterCommandRun = `ffmpeg -y -i {$in} -acodec libmp3lame -ar 44100 -ac 1 -vcodec libx264 -vf scale=640:-1 -fs 50M {$out}`;
+        $outputAfterCommandRun = `ffmpeg -y -i {$in} -vcodec libx264 -vf scale=640:-1 -fs 50M {$out}`;
 
         if (filesize($out) > 100 && (filesize($out) < filesize($in))) {
             $this->objectWithMovie->setMovieFileName($convertedVideoName);
